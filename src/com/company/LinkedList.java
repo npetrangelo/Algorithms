7fb077cd5c538339ce492782fab14677b0434400
Nathan Petrangelo <nathan@petrangelo.org>
2022-08-24 12:03:49 -0400

    Primes

---
+++ src/com/company/LinkedList.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.util.Iterator;
+
+class Node<E> {
+    E value;
+    Node next;
+
+    public Node(E value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public Node(E value) {
+        this(value, null);
+    }
+}
+
+public class LinkedList<E> implements Iterable<E> {
+    Node<E> head;
+    int length = 0;
+
+    public LinkedList() {
+
+    }
+
+    void add(E value) {
+        head = new Node(value, head);
+        length++;
+    }
+
+    E get(int n) throws IndexOutOfBoundsException {
+        if (n >= length) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node<E> node = this.head;
+        for (int i = 0; i < n; i++) {
+            node = node.next;
+        }
+        return node.value;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator() {
+            Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null && current.next != null;
+            }
+
+            @Override
+            public E next() {
+                current = current.next;
+                return current.value;
+            }
+        };
+    }
+}
---
+++ src/com/company/Primes.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Primes {
+    static LinkedList<Integer> primes = new LinkedList<>();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                primes.add(i);
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        for (int prime : primes) {
+            if (n % prime == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
