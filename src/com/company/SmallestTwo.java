89254daff9572defca83d38c56eb0f68bf82b5fa
Nathan Petrangelo <nathan@petrangelo.org>
2022-08-31 12:18:33 -0400

    Don't need to store separate array

--- src/com/company/SmallestTwo.java
+++ src/com/company/SmallestTwo.java
@@ -3,20 +3,14 @@ package com.company;
 import java.util.Scanner;
 
 public class SmallestTwo {
-    static int[] arr;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
-        arr = new int[size];
-
-        for (int i = 0; (i < size && scanner.hasNextInt()); i++) {
-            arr[i] = scanner.nextInt();
-        }
-
         int smallest = size;
         int secondSmallest = size;
 
-        for (int n : arr) {
+        for (int i = 0; i < size && scanner.hasNextInt(); i++) {
+            int n = scanner.nextInt();
             if (n < smallest) {
                 secondSmallest = smallest;
                 smallest = n;
