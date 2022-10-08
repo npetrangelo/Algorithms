abc042a7123cce3652289672ba3e1b8d26107778
Nathan Petrangelo <nathan@petrangelo.org>
2022-10-07 23:19:42 -0400

    Time testing in Main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,31 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
-        System.out.println("Hello World");
-        int[] arr = {6,5,3,2,6,78,8,6,4,23};
-        Utils.mergesort(arr);
-        System.out.println(Arrays.toString(arr));
-
-        int[] foo = {6,5,3,2,6,78,8,6,4,23};
-        System.out.println(Utils.selectRand(foo, foo.length/2));
+        Random random = new Random();
+        int[] sizes = {20, 40, 60, 80, 100, 120, 140, 160, 180, 200};
+        int bound = 1000;
+        List<int[]> inputs = new ArrayList<>();
+        System.out.println("Dynamic Programming");
+        for (int size : sizes) {
+            int[] A = random.ints(size, 0, bound).toArray();
+            inputs.add(A);
+            long start = System.nanoTime();
+            Utils.longestIncSubSeq(A);
+            long end = System.nanoTime();
+            System.out.println(end - start);
+        }
+        System.out.println("Recursive");
+        for (int[] A : inputs) {
+            long start = System.nanoTime();
+            for (int i = 0; i < A.length; i++) {
+                LongestIncreasingSubseqRecursive.incrSubseqRecursive(i, A);
+            }
+            long end = System.nanoTime();
+            System.out.println(end - start);
+        }
     }
 }
