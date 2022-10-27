de262fc5d7e7a50806aee41f948b677f16f92891
Nathan Petrangelo <nathan@petrangelo.org>
2022-10-27 18:54:19 -0400

    Increasing subsequence gap reconstruction

--- src/IncrSubseqGap.java
+++ src/IncrSubseqGap.java
@@ -1,3 +1,5 @@
+import util.LinkedList;
+
 import java.util.Scanner;
 
 public class IncrSubseqGap {
@@ -10,19 +12,31 @@ public class IncrSubseqGap {
             a[i] = scanner.nextInt();
         }
         int[] S = new int[n];
+        LinkedList<Integer>[] L = new LinkedList[n];
         S[0] = a[0];
+        L[0] = new LinkedList<>();
+        L[0].add(a[0]);
         int max = 0;
+        LinkedList<Integer> reconstruction = new LinkedList<>();
         for (int i = 1; i < n; i++) {
             int maxSum = 0;
+            LinkedList<Integer> maxList = new LinkedList();
             // Out of the p previous sums, take the largest and add the current element to it
             for (int j = i-1; j >= 0 && j >= i - p; j--) {
                 if (a[j] < a[i] && S[j] > maxSum) {
                     maxSum = S[j];
+                    maxList = L[j];
                 }
             }
             S[i] = a[i] + maxSum;
-            if (S[i] > max) max = S[i];
+            L[i] = LinkedList.shallowCopyOf(maxList);
+            L[i].add(a[i]);
+            if (S[i] > max) {
+                max = S[i];
+                reconstruction = L[i];
+            }
         }
         System.out.println(max);
+        System.out.println(reconstruction.toString());
     }
 }
