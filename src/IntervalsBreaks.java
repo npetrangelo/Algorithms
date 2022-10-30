c8049c0eafac0d2f3723640674babfc4bc6bd54c
Nathan Petrangelo <nathan@petrangelo.org>
2022-10-29 21:22:19 -0400

    Mostly working algorithm

--- src/IntervalsBreaks.java
+++ src/IntervalsBreaks.java
@@ -6,7 +6,7 @@ public class IntervalsBreaks {
         int n = scanner.nextInt();
         Interval[] intervals = new Interval[n];
         for (int i = 0; i < n; i++) {
-            intervals[i] = new Interval(scanner.nextInt(), scanner.nextInt());
+            intervals[i] = new Interval(scanner.nextInt(), scanner.nextInt(), i);
         }
         int[][] b = new int[n][n];
         for (int i = 0; i < n; i++) {
@@ -14,15 +14,24 @@ public class IntervalsBreaks {
                 b[i][j] = scanner.nextInt();
             }
         }
-
-        
+        int[] S = new int[n+1];
+        S[0] = 0;
+        Utils.mergesort(intervals, (i1, i2) -> i1.end - i2.end);
+        for (int j = 1; j <= n; j++) {
+            int k = j;
+            // Find the first k such that k ends b[k][j] units time before j starts
+            for ( ; k > 0 && intervals[j-1].start - intervals[k-1].end < b[k-1][j-1]; k--);
+            S[j] = Math.max(S[j-1], S[k]+1);
+        }
+        System.out.println(S[n]);
     }
 
     static class Interval {
-        final int start, end;
-        public Interval(int start, int end) {
+        final int start, end, bIndex;
+        public Interval(int start, int end, int bIndex) {
             this.start = start;
             this.end = end;
+            this.bIndex = bIndex;
         }
     }
 }
